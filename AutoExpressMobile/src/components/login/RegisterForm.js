5920677338df27e7662b0c5abfe941959ab5273c
ameadev <amewena@gmail.com>
2019-02-09 14:41:22 -0500

    Register validation input OK

--- AutoExpressMobile/src/components/login/RegisterForm.js
+++ AutoExpressMobile/src/components/login/RegisterForm.js
@@ -34,6 +34,7 @@ class RegisterForm extends Component {
       compteInput: "",
       compteError: "",
       passwordInput: "",
+      password2Input: "",
       passwordError: "",
       inputError: "",
       responseData: "",
@@ -42,150 +43,173 @@ class RegisterForm extends Component {
   }
 
   customerRegister () {
-    var formData = new FormData();
-    formData.append("customerFistName", "Az");
-    formData.append("customerLastName", "Az");
-    formData.append("customerPhoneNumber", "1233444");
-    formData.append("customerEMailAddress", "mab@hh");
-    formData.append("customerLogin", "az");
-    formData.append("customerPassword", "az");
+    //last validation 
+    this.inputControle(this.state.prenomInput, 'prenomInput');
+    this.inputControle(this.state.nomInput, 'nomInput');
+    this.inputControle(this.state.telephoneInput, 'telephoneInput');
+    this.inputControle(this.state.emailInput, 'emailInput');
+    this.inputControle(this.state.compteInput, 'compteInput');
+    this.inputControle(this.state.passwordInput, 'passwordInput');
+    this.inputControle(this.state.password2Input, 'password2Input');
 
-    var registerData = JSON.stringify({
-      customerFistName: this.state.prenomInput,
-      customerLastName: this.state.nomInput,
-      customerPhoneNumber: this.state.telephoneInput,
-      customerEMailAddress: this.state.emailInput,
-      customerLogin: this.state.compteInput,
-      customerPassword: this.state.passwordInput
-    });
+    if (this.state.errorInput === 0) {
+      var registerData = JSON.stringify({
+        customerFistName: this.state.prenomInput,
+        customerLastName: this.state.nomInput,
+        customerPhoneNumber: this.state.telephoneInput,
+        customerEMailAddress: this.state.emailInput,
+        customerLogin: this.state.compteInput,
+        customerPassword: this.state.passwordInput
+      });
 
-    fetch('http://autoexpress.gabways.com/api/customer.php', {
-    method: 'POST',
-    headers: { 
-     'Accept': 'application/json', 
-     'Content-Type': 'application/json',
-    },
-    body: registerData
+      /*var registerData = JSON.stringify({
+        customerFistName: 'De',
+        customerLastName: 'De',
+        customerPhoneNumber: '5544778',
+        customerEMailAddress: 'De',
+        customerLogin: 'de',
+        customerPassword: 'de'
+      });*/
 
-    }).then((response) => response.json())
-      .then((responseJson) => { 
-        this.setState({ responseData: responseJson }); 
+      fetch('http://autoexpress.gabways.com/api/customer.php', {
+      method: 'POST',
+      headers: { 
+       'Accept': 'application/json', 
+       'Content-Type': 'application/json',
+      },
+      body: registerData
 
-      })
-      .catch((err) => { this.setState({ responseError: err }); });
+      }).then((response) => response.json())
+        .then((responseJson) => { 
+          this.setState({ responseData: responseJson }); 
+          if (responseJson.errorMessage) {
+           alert (responseJson.errorMessage);
+          }
+          else {
+            alert ("Message de confirmation");
+            this.resetAllInput();
+          }
+        })
+        .catch((err) => { this.setState({ responseError: err });});
 
-        
-    alert ("Message de confirmation");
-
+    }
   }
 
   inputControle(inputValue, inputName){
+    var errorInput = '0';
     switch(inputName) {
+      
       case 'prenomInput':
+      var prenomError = '';
         if(!inputValue){
-          this.setState({
-            errorInput: 'error',
-            prenomError: 'Prenom requis',
-          })
+          prenomError = 'Prenom requis';
+          errorInput = '1';
         }
-        else {
-          this.setState({
-            errorInput: '',
-            prenomError: '',
+        this.setState({
+            errorInput: errorInput,
+            prenomError: prenomError,
             prenomInput: inputValue
-          })
-        } 
+          });
         break;
+
       case 'nomInput':
+      var nomError = '';
         if(!inputValue){
-          this.setState({
-            errorInput: 'error',
-            nomError: 'Prenom requis',
-          })
+          errorInput = '1';
+          nomError = 'Prenom requis';
         }
-        else {
-          this.setState({
-            errorInput: '',
-            nomError: '',
+        this.setState({
+            errorInput: errorInput,
+            nomError: nomError,
             nomInput: inputValue
           })
-        } 
         break;
+
       case 'telephoneInput':
+      var telephoneError = '';
         if(!inputValue){
-          this.setState({
-            errorInput: 'error',
-            telephoneError: 'Telephone requis',
-          })
+          errorInput = '1';
+          telephoneError = 'Telephone requis';
         }
-        else {
-          this.setState({
-            errorInput: '',
-            telephoneError: '',
+        this.setState({
+            errorInput: errorInput,
+            telephoneError: telephoneError,
             telephoneInput: inputValue
-          })
-        } 
+          });
         break;
+
       case 'emailInput':
-        if(!inputValue || inputValue.includes("@")){
+      var emailError = '';
+        if(!inputValue || !inputValue.includes("@")) {
+          emailError = 'Email requis ou incorrect';
+          inputError = '1';
           this.setState({
             errorInput: 'error',
-            emailError: 'Email requis ou incorrect',
+            emailError: 'Email requis ',
           })
         }
-        else {
-          this.setState({
-            errorInput: '',
-            emailError: '',
+        this.setState({
+            errorInput: errorInput,
+            emailError: emailError,
             emailInput: inputValue
-          })
-        } 
+          });
         break;
+
       case 'compteInput':
+      var compteError = '';
         if(!inputValue){
-          this.setState({
-            errorInput: 'error',
-            compteError: 'Compte requis',
-          })
+          errorInput = '1';
+          compteError = 'Compte requis';
         }
-        else {
-          this.setState({
-            errorInput: '',
-            compteError: '',
+        this.setState({
+            errorInput: errorInput,
+            compteError: compteError,
             compteInput: inputValue
-          })
-        } 
+          });
         break;
+
       case 'passwordInput':
+      var passwordError = '';
         if(!inputValue){
-          this.setState({
-            errorInput: 'error',
-            passwordError: 'Mot de passe requis',
-          })
+          errorInput = '1';
+          passwordError = 'Mot de passe requis';
         }
-        else {
-          this.setState({
-            errorInput: '',
-            passwordError: '',
+        this.setState({
+            errorInput: errorInput,
+            passwordError: passwordError,
             passwordInput: inputValue
           })
-        } 
         break;
+
       case 'password2Input':
+      var passwordError = '';
         if(!inputValue || inputValue != this.state.passwordInput){
-          this.setState({
-            errorInput: 'error',
-            passwordError: 'Erreur confirmation mot de passe',
-          })
+          errorInput = '1';
+          passwordError = 'Erreur confirmation mot de passe';
         }
         else {
-          this.setState({
-            errorInput: '',
-            passwordError: ''
-          })
+          passwordError = '';
         } 
+        this.setState({
+            errorInput: errorInput,
+            passwordError: passwordError,
+            password2Input: inputValue
+          })
         break;
+        
     } 
+  }
+
+  resetAllInput() {
+    this.state = {
+      nomInput: "",
+      prenomInput: "",
+      telephoneInput: "",
+      emailInput: "",
+      compteInput: "",
+      passwordInput: "",
+      password2Input: ""
+    };
   }
 
   render() {
@@ -204,6 +228,7 @@ class RegisterForm extends Component {
             <TextInput style={Styles.inputProfile}
               placeholder="Entrer votre Prénom ..."
               returnKeyType="default"
+              value={this.state.prenomInput}
               onChangeText={(text) => {
                 this.inputControle(text, 'prenomInput');
               }}
@@ -217,6 +242,7 @@ class RegisterForm extends Component {
             <TextInput style={Styles.inputProfile}
               placeholder="Entrer votre Nom ..."
               returnKeyType="default"
+              value={this.state.nomInput}
               onChangeText={(text) => {
                 this.inputControle(text, 'nomInput');
               }}
@@ -226,9 +252,11 @@ class RegisterForm extends Component {
             />
 
             <Text style={Styles.smallTitleMove}>NUMÉRO DE TELEPHONE <Text style={Styles.errorTitleRed}>*</Text> </Text>
+            <Text style={Styles.errorTitleRed}>{this.state.telephoneError}</Text>
             <TextInput style={Styles.inputProfile}
               placeholder="Entrer votre numéro de télephone ..."
               returnKeyType="default"
+              value={this.state.telephoneInput}
               onChangeText={(text) => {
                 this.inputControle(text, 'telephoneInput');
               }}
@@ -242,6 +270,7 @@ class RegisterForm extends Component {
             <TextInput style={Styles.inputProfile}
               placeholder="Entrer votre E-Mail ..."
               returnKeyType="default"
+              value={this.state.emailInput}
               keyboardType = "email-address"
               onChangeText={(text) => {
                 this.inputControle(text, 'emailInput');
@@ -256,6 +285,7 @@ class RegisterForm extends Component {
             <TextInput style={Styles.inputProfile}
               placeholder="Entrer votre compte ..."
               returnKeyType="default"
+              value={this.state.compteInput}
               onChangeText={(text) => {
                 this.inputControle(text, 'compteInput');
               }}
@@ -272,6 +302,7 @@ class RegisterForm extends Component {
               secureTextEntry = {true}
               autoCapitalize="none"
               autoCorrect={false}
+              value={this.state.passwordInput}
               onChangeText={(text) => {
                 this.inputControle(text, 'passwordInput');
               }}
@@ -286,6 +317,7 @@ class RegisterForm extends Component {
               secureTextEntry = {true}
               autoCapitalize="none"
               autoCorrect={false}
+              value={this.state.password2Input}
               onChangeText={(text) => {
                 this.inputControle(text, 'password2Input');
               }}
@@ -294,7 +326,6 @@ class RegisterForm extends Component {
               }}
             />
 
-            <Text style={Styles.errorTitleRed}>{this.state.prenomInput}</Text>
             <View style={Styles.buttonViewContainer}>
               <TouchableOpacity style={Styles.buttonContainer} onPress={this.customerRegister.bind(this)}>
                 <Text style={Styles.buttonText}>CREER COMPTE</Text>
